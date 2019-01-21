f215563fd6c2be196d3b4b0a776e67a803ff0514
Álvaro Salcedo <alvaro.sg93@gmail.com>
2019-01-21 18:37:12 +0100

    Codacy integration with Travis

---
+++ README.md
@@ -0,0 +1,22 @@
+# Pressurizer
+
+> Backend of Pressurizer
+
+---
+
+| *Branch*    | *Build*             | *Coverage*             | *Quality*             |
+| ----------- | :-----------------: | :--------------------: | :-------------------: |
+| **master**  | [![mBuild]][travis] | [![mCoverage]][codacy] | [![mQuality]][codacy] |
+| **develop** | [![dBuild]][travis] | [![dCoverage]][codacy] | [![dQuality]][codacy] |
+
+---
+
+[travis]: https://travis-ci.com/alvr/pressurizer-server
+[codacy]: https://app.codacy.com/project/alvr/pressurizer-server
+
+[mBuild]: https://img.shields.io/travis/com/alvr/pressurizer-server/master.svg "Build on master"
+[dBuild]: https://img.shields.io/travis/com/alvr/pressurizer-server/develop.svg "Build on develop"
+[mCoverage]: https://img.shields.io/codacy/coverage/e503dabc76eb4aa8adbe512495373e74/master.svg "Coverage on master"
+[dCoverage]: https://img.shields.io/codacy/coverage/e503dabc76eb4aa8adbe512495373e74/develop.svg "Coverage on develop"
+[mQuality]: https://img.shields.io/codacy/grade/e503dabc76eb4aa8adbe512495373e74/master.svg "Quality on master"
+[dQuality]: https://img.shields.io/codacy/grade/e503dabc76eb4aa8adbe512495373e74/develop.svg "Quality on develop"
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,4 @@
+import com.adarshr.gradle.testlogger.theme.ThemeType
 import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
@@ -10,10 +11,14 @@ object Versions {
     const val LOGBACK = "1.2.3"
 }
 
+val codacy: Configuration by configurations.creating
+
 plugins {
     application
+    jacoco
     kotlin("jvm") version "1.3.11"
     id("com.github.johnrengelman.shadow") version "4.0.3"
+    id("com.adarshr.test-logger") version "1.6.0"
 }
 
 repositories {
@@ -26,10 +31,16 @@ dependencies {
     implementation("io.ktor", "ktor-server-netty", Versions.KTOR)
 
     implementation("ch.qos.logback", "logback-classic", Versions.LOGBACK)
+
+    codacy("com.github.codacy:codacy-coverage-reporter:-SNAPSHOT")
 }
 
 application {
     mainClassName = "me.alvr.pressurizer.PressurizerKt"
+}
+
+jacoco {
+    toolVersion = "0.8.2"
 }
 
 tasks {
@@ -41,5 +52,35 @@ tasks {
         archiveBaseName.set("pressurizer")
         archiveClassifier.set("")
         archiveVersion.set("")
+    }
+
+    withType<Test> {
+        useJUnitPlatform()
+        testlogger {
+            theme = ThemeType.MOCHA
+        }
+
+        val jacocoReport = withType<JacocoReport> {
+            reports {
+                xml.isEnabled = true
+            }
+        }
+
+        finalizedBy(jacocoReport)
+    }
+
+    register<JavaExec>("uploadReportCodacy") {
+        main = "com.codacy.CodacyCoverageReporter"
+        classpath = codacy
+
+        args = listOf(
+            "report",
+            "-l",
+            "Kotlin",
+            "-r",
+            "$buildDir/reports/jacoco/test/jacocoTestReport.xml",
+            "--prefix",
+            "src/main/kotlin/"
+        )
     }
 }
