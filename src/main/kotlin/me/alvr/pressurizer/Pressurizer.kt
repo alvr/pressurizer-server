93f0b3c6beb8ad3d6714dc4792dae1f33225bab6
Álvaro Salcedo <alvaro.sg93@gmail.com>
2019-01-27 13:16:39 +0100

    Install authentication feature for Ktor

--- src/main/kotlin/me/alvr/pressurizer/Pressurizer.kt
+++ src/main/kotlin/me/alvr/pressurizer/Pressurizer.kt
@@ -2,15 +2,33 @@ package me.alvr.pressurizer
 
 import io.ktor.application.Application
 import io.ktor.application.call
+import io.ktor.application.install
+import io.ktor.auth.Authentication
+import io.ktor.auth.jwt.jwt
 import io.ktor.response.respondText
 import io.ktor.routing.get
 import io.ktor.routing.routing
 import io.ktor.server.engine.embeddedServer
 import io.ktor.server.netty.Netty
+import me.alvr.pressurizer.auth.AuthJWT
+import me.alvr.pressurizer.auth.SteamId
 import me.alvr.pressurizer.config.ServerSpec
 import me.alvr.pressurizer.config.config
 
+/**
+ * Configuration of the server itself.
+ *
+ * @receiver Application of type [Application]
+ */
 fun Application.pressurizer() {
+    install(Authentication) {
+        jwt {
+            verifier(AuthJWT.verifier)
+            validate {
+                SteamId(it.payload.getClaim("id").asString())
+            }
+        }
+    }
     routing {
         get("/") {
             call.respondText { "Pressurizer" }
@@ -18,6 +36,9 @@ fun Application.pressurizer() {
     }
 }
 
+/**
+ * Main entry point
+ */
 fun main() {
     embeddedServer(
         Netty,
