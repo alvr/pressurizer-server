d09f4dac4955ca506150e05a11cae9c40d0a42a9
Álvaro Salcedo <alvaro.sg93@gmail.com>
2019-02-23 15:27:48 +0100

    Rename columns

--- src/main/kotlin/me/alvr/pressurizer/database/tables/UserGamesTable.kt
+++ src/main/kotlin/me/alvr/pressurizer/database/tables/UserGamesTable.kt
@@ -5,15 +5,15 @@ import org.jetbrains.exposed.sql.Table
 /**
  * Database table for countries.
  *
- * [user] Steam64Id of the user.
- * [game] identification of the game.
+ * [steamId] Steam64Id of the user.
+ * [appId] identification of the game.
  * [cost] price the user paid for the game.
  * [timePlayed] total minutes played.
  * [finished] true if the user finished the game.
  */
 object UserGamesTable : Table("user_games") {
-    val user = (varchar("owner", 20) references UsersTable.steamId).index()
-    val game = varchar("game", 8) references GamesTable.appId
+    val steamId = (varchar("steam_id", 20) references UsersTable.steamId).index()
+    val appId = varchar("app_id", 8) references GamesTable.appId
     val cost = decimal("cost", 6, 2)
     val timePlayed = integer("time_played")
     val finished = bool("finished").default(false)
